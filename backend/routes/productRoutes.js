33b609fa66d509b2e52e783b04e660ba707bdd72
ThyaraV <thyara.vintimilla@udla.edu.ec>
2023-11-17 11:49:17 -0500

    Arreglo middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import Product from '../models/productModel.js';
 
 router.get('/',asyncHandler(async(req,res)=>{
     const products = await Product.find({});
+    //throw new Error('Some error');
     res.json(products);
 }));
 
@@ -15,7 +16,8 @@ router.get('/:id',asyncHandler(async(req,res)=>{
     if(product){
         res.json(product);
     }
-    res.status(404).json({message:'Product not found'});
+    res.status(404);
+    throw new Error('Resource not found');
 }));
 
 
