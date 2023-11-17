f85cb046f5cd456be121e5f3bd2b3a66ec0aa817
ThyaraV <thyara.vintimilla@udla.edu.ec>
2023-11-17 14:20:39 -0500

    Mensajes para aviso de errores

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,10 @@ import{Link} from 'react-router-dom';
 import {Row, Col,Image,ListGroup,Card,Button, ListGroupItem} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useGetProductsDetailsQuery } from '../slices/productsApiSlice.js';
+import Loader from '../components/Loader.jsx';
+import Message from '../components/message.jsx';
+
+
 const ProductScreen = () => {
     const{id:productId}=useParams();
     const {data:product,isLoading,error}=useGetProductsDetailsQuery(productId);
@@ -14,9 +18,11 @@ const ProductScreen = () => {
         </Link>
 
         {isLoading? (
-            <h2>Loading...</h2>
+            <Loader/>
         ) : error ?(
-            <div>{error?.data?.message || error.error}</div>
+            <Message variant='danger'>
+                {error?.data?.message || error.error
+            }</Message>
         ):(
             <Row>
             <Col md={5}>
